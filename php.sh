a70e114255f0a08a9e114d2ccaf4f332d277e6d8
Tony Phipps <tonyphipps@gmail.com>
2017-02-26 01:40:31 +0000

    feat: add OSX prep shell script and php, mysql and valet script

---
+++ php.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Install PHP environment packages
+brew install homebrew/php/php71
+brew install mysql
+
+# Start mysql server
+brew services start mysql
+
+# Install composer globally
+php composer-setup.php --install-dir=bin --filename=composer
+mv composer.phar /usr/local/bin/composer
+
+# Install Valet via composer
+composer global require laravel/valet
+
+# Initiate Valet
+mkdir ~/valet
+cd ~/valet
+valet park
