63198a8e53139e96a5ecb5f5fd59910500650854
Tony Phipps <tonyphipps@gmail.com>
2023-11-27 00:19:53 +0000

    fix: ensure tmux script installs tmux-thumbs plugin

--- tmux.sh
+++ tmux.sh
@@ -28,3 +28,6 @@ brew install tmux
 
 # Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install tmux-thumbs plugin
+git clone https://github.com/fcsonline/tmux-thumbs ~/.tmux/plugins/tmux-thumbs
